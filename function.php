4d82a6181cf68baa63681bf9db9aeb331797f8b1
Zurab Arutyunov <SpanihBob@gmail.com>
2022-04-16 23:42:55 +0400

    first commit

---
+++ function.php
@@ -0,0 +1,108 @@
+<?php
+    $products = 'product.txt';
+    class Microwave
+    {
+        public $power;              //мощность
+        public $time;               //время
+        public $items = array();    //массив для хранения инфо о продукте
+
+    // function __construct($power, $time){       //функция __construct задает начальные значения класса
+    //     $this -> power = $power;
+    //     $this -> time = $time;
+    // }
+
+    function getItems()             //возвращаем items
+    {
+        global $products;      //область видимости
+        $file=fopen($products,'r');           //(fopen) Открывает файл (r) для чтения
+        while($line=fgets($file, 128)) {        //fgets — Читает строку из файла
+            $readProduct=substr($line,0,strpos($line,':'));              //вытаскиваем название продукта
+            $newLine1 = str_replace($readProduct. ':', '', $line);          //убираем имя пользователя из строки            
+            $readTime=trim(substr($newLine1,0,strpos($newLine1,':')));
+            $readPower = str_replace($readTime. ':', '', $newLine1);     //убираем имя пользователя из строки            
+            array_push($this->items, [$readProduct, $readTime, $readPower]);
+        }
+        return $this -> items;
+    }
+
+    function getTime()              //возвращаем time
+    {
+        return $this -> time;
+    }
+
+    function getPower()             //возвращаем power
+    {
+        return $this -> power;
+    }
+    
+    function setProduct($product, $time, $power)
+    {
+        global $products;      //область видимости
+        $file=fopen($products,'r');           //(fopen) Открывает файл (r) для чтения
+        while($line=fgets($file, 128)) {        //fgets — Читает строку из файла
+            $readProduct=substr($line,0,strpos($line,':'));              //вытаскиваем название продукта
+            $newLine1 = str_replace($readProduct. ':', '', $line);          //убираем имя пользователя из строки            
+            $readTime=trim(substr($newLine1,0,strpos($newLine1,':')));
+            $readPower = str_replace($readTime. ':', '', $newLine1);     //убираем имя пользователя из строки            
+            array_push($this->items, [$readProduct, $readTime, $readPower]);
+        }        
+    }
+
+    function setTime($time)         //устанавливаем time
+    {
+        $this -> time = $time;
+    }
+
+    function setPower($power)         //устанавливаем power
+    {
+        $this -> power = $power;
+    }
+}
+
+     //функция проверки формы
+    function enter($product, $time, $power) 
+    {
+        $time=trim(htmlspecialchars($time));                        //trim — Удаляет пробелы (или другие символы) из начала и конца строки
+        $power=trim(htmlspecialchars($power));                      //htmlspecialchars — Преобразует специальные символы в HTML-сущности
+        $product=trim(htmlspecialchars($product));    
+            if($time && $power)
+            {
+                return true;
+            } 
+            return false;
+    }
+
+      //функция проверки формы для добавления нового продукта
+    function add($product, $time, $power) 
+    {
+        $product=trim(htmlspecialchars($product));                 
+        $time=trim(htmlspecialchars($time));                        //trim — Удаляет пробелы (или другие символы) из начала и конца строки
+        $power=trim(htmlspecialchars($power));                      //htmlspecialchars — Преобразует специальные символы в HTML-сущности
+
+        //блок проверки уникальности продукта
+        global $products;      //область видимости
+        $file=fopen($products,'a+');           //(fopen) Открывает файл (a+) для записи и чтения
+        while($line=fgets($file, 128)) {        //fgets — Читает строку из файла
+            $readProduct=substr($line,0,strpos($line,':'));              //mb_substr — Возвращает часть строки, strpos — Находит позицию первого вхождения подстроки в строку
+            if($readProduct == $product) {
+                return false;
+            }
+        }
+
+        //блок добавления нового продукта
+        $line=$product.':'.$time.':'.$power."\r\n";   
+        fputs($file,$line);     //fputs — Псевдоним fwrite() — Бинарно-безопасная запись в файл
+        fclose($file);          //fclose — Закрывает открытый дескриптор файла
+        return true;
+
+        if($product && $time && $power)
+            {
+                return true;
+            } 
+            else 
+            {
+                return false;
+            }
+    }
+        
+?>
